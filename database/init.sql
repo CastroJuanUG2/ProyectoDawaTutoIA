054535043ce8b2f23780488f58476f9c252cdcb7
Dean Leon <djleonpaucar@gmail.com>
2026-07-02 01:15:02 -0700

    chore: define project base structure

---
+++ database/init.sql
@@ -0,0 +1,14 @@
+﻿-- =====================================================
+-- ProyectoDawaTutoIA
+-- Script inicial de base de datos
+-- Sistema Web Inteligente de Gestión de Tutorías Académicas con Agente de IA
+-- =====================================================
+
+-- Este archivo será utilizado para definir la estructura inicial
+-- de la base de datos PostgreSQL del proyecto.
+
+-- Las tablas principales se agregarán en la fase de diseño de base de datos.
+
+-- Nota:
+-- Este script será ejecutado automáticamente por Docker cuando se levante
+-- el contenedor de PostgreSQL por primera vez.
